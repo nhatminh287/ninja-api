f31b52bcb4de090b7d66044f70a9659c69f5a9c7
nhatminh287 <nhatminhpro2001@gmail.com>
2024-08-12 16:38:36 +0700

    health

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { LoggerModule } from '@app/common';
+import { HealthModule, LoggerModule } from '@app/common';
 import { JwtModule } from '@nestjs/jwt';
 import * as Joi from 'joi';
 import { AuthController } from './auth.controller';
